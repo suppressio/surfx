6b119a45a2134d414bd682730fecf07c3b763f05
suppressio <daniele@suppressio.net>
2022-05-15 23:13:46 +0200

    add generic grid

--- src/app/main-grid/main-grid.component.ts
+++ src/app/main-grid/main-grid.component.ts
@@ -1,24 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+// let data: any[] = [
+//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+//   {position: 9, surename: 'Fluorine', weight: 18.9984, symbol: 'F'},
+//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+// ];
 
 @Component({
   selector: 'app-main-grid',
@@ -26,15 +19,30 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./main-grid.component.scss']
 })
 export class MainGridComponent implements OnInit {
+  @Input() data:any;
+  
+  displayedColumns: string[] = [];
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-
-  dataSource = ELEMENT_DATA;
+  dataSource:any;
 
   constructor() { }
 
+
   ngOnInit(): void {
-    this.dataSource = ELEMENT_DATA;
+    this.initData();
   }
+
+  initData(){
+    //let d= data[0];
+    this.data.forEach((d:any)=>{
+      Object.keys(d).forEach(k=>{
+        if(this.displayedColumns.indexOf(k)<0)
+          this.displayedColumns.push(k);
+      });
+    });
+    console.log("displayedColumns:", this.displayedColumns);
+    this.dataSource = this.data;
+  }
+
 
 }
